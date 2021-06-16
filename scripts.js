78d3c0630ab4209ef0f35f45aca77c68f81c0d05
jr3564 <rodolfojr94@gmail.com>
2021-06-16 15:02:29 -0300

    Elementros posicionados

--- scripts.js
+++ scripts.js
@@ -1,14 +1,12 @@
-import data from './data/logos.js';
+import data from './data/index.js';
 
 const generateLogoAnchor = (atributes) => {
   const a = document.createElement('a');
   a.setAttribute('href', atributes.href);
-  a.setAttribute('class', atributes.class.a);
 
   const img = document.createElement('img');
   img.setAttribute('src', atributes.src);
-  img.setAttribute('class', atributes.class.img);
-  img.setAttribute('alt', atributes.alt || '');
+  img.setAttribute('alt', atributes.alt || 'Logo');
 
   a.appendChild(img);
 
@@ -17,7 +15,7 @@ const generateLogoAnchor = (atributes) => {
 
 const injectList = (log) => {
   const li = document.createElement('li');
-  const a = generateLogoAnchor({ ...log, class: { img: 'logoImage', a: 'logoA' } });
+  const a = generateLogoAnchor(log);
   li.appendChild(a);
 
   document.querySelector('.logoList').appendChild(li);
